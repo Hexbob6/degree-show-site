e374dbddf35f8ccfea2e4fad16ab02e00af0f35d
Hexbob6 <sam.walker.email@gmail.com>
2018-03-29 22:03:32 +0100

    Added the Holding Page
    Added the holding page and associated code. It can be accessed by adding '/holding.html' to the end of the homepage site link.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,18 @@
+var $;
 var console;
 
-// Displays the loading icon until page is fully loaded
+
 $(document).ready(function () {
     
+    // Displays the loading icon until page is fully loaded
     "use strict";
     $("#loading-overlay").fadeOut(100);
     $('#loading-box').css({'transform' : 'translateY(50%)'});
+    
+    
+
+    
+
     
 });
 
@@ -146,11 +153,14 @@ $(document).click(function () {
 
 $(document).ready(function () {
     
+    
+    // Variables for custom course content
     "use strict";
     var pageBody = $('body'),
         headerVideo = $('.student-header-video-wrapper'),
         studentSpecialsims = $('.student-specialisms');
     
+
     var linesLeftCDME =
         '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/CDME_page-lines-left_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/CDME_student_lines_left1.png">',
         linesRightCDME =
@@ -189,6 +199,8 @@ $(document).ready(function () {
     // - graphics
     // - fine-art
     
+    
+    // If body has only one class, do this, or if more than one class do this
     if (pageBody.hasClass('cdme') && pageBody[0].classList.length === 1) {
         
         headerVideo.append(linesLeftCDME, linesRightCDME);
@@ -297,10 +309,11 @@ $(document).ready(function () {
         
     }
     
-    // Initiate as 'False'
-    var isMobile = false;
     
-    // Device Detection
+    // Mobile detection
+    
+    var isMobile = false; //Initiate as false
+    
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|iP(hone|od)|iPad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
 
         isMobile = true;
@@ -308,6 +321,7 @@ $(document).ready(function () {
     
     var mobileScreenWidth = $(window).width();
     
+    // Checks to ensure device is mobile but not a tablet (iPad) 
     if (isMobile && (mobileScreenWidth < 768)) {
         
 //        alert('This is mobile');
@@ -321,12 +335,47 @@ $(document).ready(function () {
         $(".mobile-header-lines>img").css({'display': 'block'});
         
         
-        $('.screen-size p').append("<span>This is mobile</span>");
+//        $('.screen-size p').append("<span>This is mobile</span>");
         
     } else {
         
-        $('.screen-size span').html("This is not mobile.");
+//        $('.screen-size span').html("This is not mobile.");
 //        alert('This isn\'t mobile');
     }
     
 });
+
+
+// Countdown timer for holding page
+$(window).on('load', function(e) {
+
+    var countDownDate = new Date("May 18, 2018 18:00:00").getTime();
+    
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        $('#countdown').html(days + "d " + hours + "h "
+                             + minutes + "m " + seconds + "s ");
+
+        // If the count down is finished, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            $('#countdown').html("The Degree Show is now open.");
+        }
+    }, 1000);
+    
+})
+
