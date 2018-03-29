cf66727a01c7ddb4042761d57431e4b5369c0f92
Hexbob6 <sam.walker.email@gmail.com>
2018-03-29 14:05:18 +0100

    Updating responsive code
    Image swapping doesn't work on mobile, trying to figure out why.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -144,42 +144,6 @@ $(document).click(function () {
 });
 
 
-
-
-
-$(document).ready(function () {
-        
-    "use strict";
-    var isMobile = false; // Initiate as 'False'
-
-    // Device Detection
-    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
-            
-        isMobile = true;
-    }
-    
-    var mobileScreenWidth = $(window).width();
-    
-    if (isMobile && (mobileScreenWidth <= 767)) {
-        
-        // Makes header videos display as images
-        $(".header-content-video video").css({'display': 'none'});
-        $(".header-content-video img").css({'display': 'block'});
-        $(".student-header-video-wrapper video").css({'display': 'none'});
-        $(".video-replacement-image").css({'display': 'block'});
-        $(".header-content-video>video").css({'display': 'none'});
-        $(".header-content-video>img").css({'display': 'block'});
-        
-        
-        $('.screen-size p').append("<span>This is mobile</span>");
-        
-    } else {
-        $('.screen-size span').html("This is not mobile.");
-    }
-    
-});
-
-
 $(document).ready(function () {
     
     "use strict";
@@ -188,33 +152,33 @@ $(document).ready(function () {
         studentSpecialsims = $('.student-specialisms');
     
     var linesLeftCDME =
-        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/CDME_page-lines-left_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/CDME_student_lines_left1.png">',
+        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/CDME_page-lines-left_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/CDME_student_lines_left1.png">',
         linesRightCDME =
-        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/CDME_page-lines-right_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/CDME_student_lines_right1.png">',
+        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/CDME_page-lines-right_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/CDME_student_lines_right1.png">',
         cdmeSpecialism = '<a href="" class="blue-font"><span>//</span>Creative Digital Media</a>';
     
     var linesLeftANIM =
-        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/ANIM_page-lines-left_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ANIM_student_lines_left1.png">',
+        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/ANIM_page-lines-left_vid1.mp4" type="video/mp4"></video' + '><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ANIM_student_lines_left1.png">',
         linesRightANIM =
-        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/ANIM_page-lines-right_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ANIM_student_lines_right1.png">',
+        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/ANIM_page-lines-right_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ANIM_student_lines_right1.png">',
         animSpecialism = '<a href="" class="green-font"><span>//</span>Animation</a>';
         
     var linesLeftGDES =
-        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/GDES_page-lines-left_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/GDES_student_lines_left1.png">',
+        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/GDES_page-lines-left_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/GDES_student_lines_left1.png">',
         linesRightGDES =
-        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/GDES_page-lines-right_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/GDES_student_lines_right1.png">',
+        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/GDES_page-lines-right_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/GDES_student_lines_right1.png">',
         gdesSpecialism = '<a href="" class="purple-font"><span>//</span>Graphic Design</a>';
     
     var linesLeftILLU =
         '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/ILLU_page-lines-left_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ILLU_student_lines_left1.png">',
         linesRightILLU =
-        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/ILLU_page-lines-right_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ILLU_student_lines_right1.png">',
+        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/ILLU_page-lines-right_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ILLU_student_lines_right1.png">',
         illuSpecialism = '<a href="" class="yellow-font"><span>//</span>Illustration</a>';
     
     var linesLeftART =
-        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/ART_page-lines-left_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ART_student_lines_left1.png">',
+        '<video class="vertical-align" autoplay><source     src="vid/Course_Lines/ART_page-lines-left_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ART_student_lines_left1.png">',
         linesRightART =
-        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/ART_page-lines-right_vid1.mp4" type="video/mp4"></video><img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ART_student_lines_right1.png">',
+        '<video class="vertical-align" autoplay><source src="vid/Course_Lines/ART_page-lines-right_vid1.mp4" type="video/mp4"></video>' + '<img class="video-replacement-image vertical-align" src="img/brand_elements/course_line_images/ART_student_lines_right1.png">',
         artSpecialism = '<a href="" class="red-font"><span>//</span>Fine Art &amp Art and Design</a>';
         
     
@@ -241,118 +205,128 @@ $(document).ready(function () {
         console.log("GDES");
         
     } else if (pageBody.hasClass('illust') && pageBody[0].classList.length === 1) {
-
+        
         headerVideo.append(linesLeftILLU, linesRightILLU);
         studentSpecialsims.append(illuSpecialism);
         
         console.log("ILLU");
-
+        
     } else if (pageBody.hasClass('fine-art') && pageBody[0].classList.length === 1) {
-
+        
         headerVideo.append(linesLeftART, linesRightART);
         studentSpecialsims.append(artSpecialism);
         
         console.log("ART");
-
+        
     } else if (pageBody.hasClass('anim') && pageBody[0].classList.length === 1) {
-
+        
         headerVideo.append(linesLeftANIM, linesRightANIM);
         studentSpecialsims.append(animSpecialism);
-
+        
         console.log("ANIM");
-
+        
     } else if ((pageBody.hasClass('anim')) && (pageBody.hasClass('illust'))) {
-
+        
         headerVideo.append(linesLeftANIM, linesRightILLU);
         studentSpecialsims.append(animSpecialism, illuSpecialism);
-
+        
         console.log("ANIM ILLU");
         
     } else if ((pageBody.hasClass('anim')) && (pageBody.hasClass('cdme'))) {
-
+        
         headerVideo.append(linesLeftCDME, linesRightANIM);
         studentSpecialsims.append(cdmeSpecialism, animSpecialism);
-
+        
         console.log("CDME ANIM");
         
     } else if ((pageBody.hasClass('anim')) && (pageBody.hasClass('graphics'))) {
-
+        
         headerVideo.append(linesLeftANIM, linesRightGDES);
         studentSpecialsims.append(animSpecialism, gdesSpecialism);
-
+        
         console.log("ANIM GDES");
         
     } else if ((pageBody.hasClass('anim')) && (pageBody.hasClass('fine-art'))) {
-
+        
         headerVideo.append(linesLeftANIM, linesRightART);
         studentSpecialsims.append(animSpecialism, artSpecialism);
-
+        
         console.log("ANIM ART");
         
     } else if ((pageBody.hasClass('illust')) && (pageBody.hasClass('cdme'))) {
-
+        
         headerVideo.append(linesLeftCDME, linesRightILLU);
         studentSpecialsims.append(cdmeSpecialism, illuSpecialism);
-
+        
         console.log("CDME ILLU");
         
     } else if ((pageBody.hasClass('illust')) && (pageBody.hasClass('graphics'))) {
-
+        
         headerVideo.append(linesLeftILLU, linesRightGDES);
         studentSpecialsims.append(illuSpecialism, gdesSpecialism);
-
+        
         console.log("ILLU GDES");
         
     } else if ((pageBody.hasClass('illust')) && (pageBody.hasClass('fine-art'))) {
-
+        
         headerVideo.append(linesLeftILLU, linesRightART);
         studentSpecialsims.append(illuSpecialism, artSpecialism);
-
+        
         console.log("ILLU ART");
         
     } else if ((pageBody.hasClass('graphics')) && (pageBody.hasClass('cdme'))) {
-
+        
         headerVideo.append(linesLeftCDME, linesRightGDES);
         studentSpecialsims.append(cdmeSpecialism, gdesSpecialism);
-
+        
         console.log("CDME GDES");
         
     } else if ((pageBody.hasClass('cdme')) && (pageBody.hasClass('fine-art'))) {
-
+        
         headerVideo.append(linesLeftCDME, linesRightART);
         studentSpecialsims.append(cdmeSpecialism, artSpecialism);
-
+        
         console.log("CDME ART");
         
     } else if ((pageBody.hasClass('graphics')) && (pageBody.hasClass('fine-art'))) {
-
+        
         headerVideo.append(linesLeftGDES, linesRightART);
         studentSpecialsims.append(gdesSpecialism, artSpecialism);
-
+        
         console.log("GDES ART");
         
     }
     
-//    if (pageBody.hasClass('cdme')) {
-//        
-//        studentSpecialsims.append(cdmeSpecialism);
-//        
-//    } else if (pageBody.hasClass('graphics')){
-//
-//        studentSpecialsims.append(gdesSpecialism);
-//
-//    } else if (pageBody.hasClass('anim')){
-//
-//        studentSpecialsims.append(animSpecialismSpecialism);
-//
-//    } else if (pageBody.hasClass('illust')){
-//
-//        studentSpecialsims.append(illuSpecialism);
-//
-//    } else if (pageBody.hasClass('fine-art')){
-//
-//        studentSpecialsims.append(artSpecialism);
-//
-//    }
+    // Initiate as 'False'
+    var isMobile = false;
+    
+    // Device Detection
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|iP(hone|od)|iPad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+
+        isMobile = true;
+    }
+    
+    var mobileScreenWidth = $(window).width();
+    
+    if (isMobile && (mobileScreenWidth < 768)) {
+        
+        alert('This is mobile');
+        
+        // Makes header videos display as images
+        $(".header-content-video video").css({'display': 'none'});
+        $(".header-content-video img").css({'display': 'block'});
+        $(".student-header-video-wrapper video").css({'display': 'none'});
+        $(".video-replacement-image").css({'display': 'block'});
+        $(".header-content-video>video").css({'display': 'none'});
+        $(".header-content-video>img").css({'display': 'block'});
+        
+        
+        $('.screen-size p').append("<span>This is mobile</span>");
+        
+    } else {
+        
+        $('.screen-size span').html("This is not mobile.");
+        alert('This isn\'t mobile');
+    }
     
 });
